f93e6b2ef2cd552b18146eb15dbe6c2ac0c41407
ktbaek <kristofferbaek@gmail.com>
2022-12-23 22:40:10 +0100

    standalone figs edit/add

--- code/standalone_figs.R
+++ code/standalone_figs.R
@@ -3,8 +3,6 @@ source("code/other_functions.R")
 
 library(tidyverse)
 
-colors_2 <- c("#E69F00", "#56B4E9")
-
 pred_names <- c(
   "2010-19" = "Baseline: 2010-19",
   "2015-19" = "Baseline: 2015-19"
@@ -15,15 +13,37 @@ death_rates_10a <- read_csv("data/death_rates_10y_age.csv")
 preds_ns <- make_prediction(death_rates_10a, "2010-19", 2010, 2019, 1.96, Age) %>%
   bind_rows(make_prediction(death_rates_10a, "2015-19", 2015, 2019, 1.96, Age))
 
+ssi_deaths <- read_csv2("data/SSI_death_age.csv")
+
+covid_deaths <- ssi_deaths %>% 
+  pivot_longer(-Age, names_to = c(NA, "Quarter_end"), values_to = "C_deaths", names_sep = "_") %>% 
+  mutate(
+    Quarter_end = ymd(Quarter_end),
+    Quarter = quarter(Quarter_end) - quarter(1),
+    Year = year(Quarter_end),
+    Year = ifelse(Quarter == 0, 2021, Year),
+    Quarter = ifelse(Quarter == 0, 4, Quarter),
+    YQ = zoo::as.yearqtr(format(paste0(Year, Quarter)), "%Y%q")
+  ) %>% 
+  group_by(Age) %>% 
+  mutate(
+    Covid = C_deaths - lead(C_deaths)
+  ) %>% 
+  filter(!is.na(Covid)) %>% 
+  select(Age, YQ, Covid)
+
+
 gglayer_2 <- list(
   geom_ribbon(aes(YQ, ymin = (conf_lo - fit) / fit * 100, ymax = (conf_hi - fit) / fit * 100), fill = "gray75", alpha = 0.3),
+  geom_ribbon(aes(YQ, ymin = ((Death_rate  - covid_rate) / fit - 1)  * 100, ymax = (Death_rate - fit) / fit * 100), fill = "#00E676", alpha = 0.7),
   geom_line(aes(YQ, (fit  - fit) / fit * 100), size = .3),
   geom_vline(xintercept = 2019.88, size = 0.4, color = "gray70"),
-  geom_line(aes(YQ, (Death_rate - fit) / fit * 100), size = .6, color = "red"),
+  geom_vline(xintercept = 2021.25, size = 0.4, color = "#00E676"),
+  geom_line(aes(YQ, (Death_rate - fit) / fit * 100), size = .6, color = "#FF3D00"),
   scale_y_continuous(limits = c(NA, NA), labels = function(x) paste0(x, " %")),
   zoo::scale_x_yearqtr(format = "%YQ%q", n = 10, expand = expansion(mult = c(0.025, .025))),
   labs(
-    subtitle = "Red lines indicate the difference between observed and expected mortality rates relative to the expected mortality rates.\nResults based on two different reference periods (2010-2019 and 2015-2019) are shown.\nGray bands indicate a 95% prediction interval.",
+    subtitle = "Red lines indicate the difference between observed and expected mortality rates relative to the expected mortality rates.\nGreen bands indicate deaths with a positive SARS-CoV-2 PCR (data available from 2021 Q2 [green vertical line]).\nResults based on two different reference periods (2010-2019 and 2015-2019) are shown.\nGray bands indicate a 95% prediction interval.",
     caption = "Method described at github.com/ktbaek/Excess-deaths-Denmark",
     y = "Change relative to baseline"),
   facet_theme,
@@ -38,7 +58,7 @@ gglayer_2 <- list(
     plot.caption = element_text(size = 8), 
     plot.subtitle = element_text(size = 8, lineheight = 1))
 )
-)
+
 
 preds_ns %>%
   unnest(c(predict, excess)) %>%
@@ -47,6 +67,8 @@ preds_ns %>%
     Year >= 2010,
     Age %in% c("40-49", "50-59", "60-69")
   ) %>%
+  left_join(covid_deaths, by = c("Age", "YQ")) %>% 
+  mutate(covid_rate = Covid / Population) %>% 
   ggplot() +
   gglayer_2 +
   labs(title = "Changes in quarterly mortality rates for age groups 40-69 in Denmark") 
@@ -61,6 +83,8 @@ preds_ns %>%
     Year >= 2010,
     Age %in% c("0-9", "10-19", "20-29", "30-39")
   ) %>%
+  left_join(covid_deaths, by = c("Age", "YQ")) %>% 
+  mutate(covid_rate = Covid / Population) %>% 
   ggplot() +
   gglayer_2 +
   labs(title = "Changes in quarterly mortality rates for age groups 0-39 in Denmark") 
@@ -75,6 +99,8 @@ preds_ns %>%
     Year >= 2010,
     Age %in% c("70-79", "80-89", "90+")
   ) %>%
+  left_join(covid_deaths, by = c("Age", "YQ")) %>% 
+  mutate(covid_rate = Covid / Population) %>% 
   ggplot() +
   gglayer_2 +
   labs(title = "Changes in quarterly mortality rates for age groups 70+ in Denmark") 
@@ -119,3 +145,63 @@ preds_ns %>%
   theme(panel.spacing.y = unit(0.7, "cm"))
 
 ggsave("figures/baseline_ns.png", width = 12, height = 16, units = "cm", dpi = 300)
+
+
+
+SSI_covid_resolved <- read_csv2("data/Deaths_o_weeks_covid_cause.csv") %>% 
+  mutate(across(everything(), as.character)) %>% 
+  pivot_longer(-Udfald, names_to = "week_year", values_to = "value") %>% 
+  mutate(
+    value = ifelse(str_detect(value, "<"), NA, value), # replace <20 with NA
+    value = as.integer(value),
+    Year = as.integer(str_sub(week_year, 5, 8)),
+    Week = as.integer(str_sub(week_year, 2, 3)),
+    Date = c19dk::week_to_date(Year, Week, day = 7), # Date for the Sunday in each week
+    Quarter = lubridate::quarter(Date), # quarter of the Sunday in each week. The quarterly values are thereby not completely precise
+    YQ = zoo::as.yearqtr(format(paste0(Year, Quarter)), "%Y%q"),
+    Udfald = case_when(
+      Udfald == "Dødstal 30 dage efter infektion (PCR)" ~ "Deaths 30 days after positive PCR",
+      Udfald == "Død af covid (DAR)" ~ "Died due to Covid",
+      Udfald == "Endnu ikke valideret" ~ "Not yet validated",
+      Udfald == "Afventer dødsattest" ~ "Awaiting death certificate",
+      Udfald == "Død med covid (DAR)" ~ "Died with Covid")
+  ) %>% 
+  group_by(YQ, Udfald) %>% 
+  summarize(value = sum(value, na.rm = TRUE))
+  
+# compare with the numbers from Deaths_over_time
+read_csv2("data/Deaths_over_time.csv") %>% 
+  mutate(
+    Date = ymd(Dato),
+    Year = year(Date),
+    Quarter = lubridate::quarter(Date),
+    YQ = zoo::as.yearqtr(format(paste0(Year, Quarter)), "%Y%q")
+  ) %>% 
+  filter(Dato != "I alt") %>% 
+  group_by(YQ) %>% 
+  summarize(Deaths_pcr = sum(Antal_døde, na.rm = TRUE))
+
+# reorder categories
+SSI_covid_resolved$Udfald <- krisr::new_order(
+    vector = SSI_covid_resolved$Udfald, order = c("Deaths 30 days after positive PCR",
+              "Died with Covid",
+              "Awaiting death certificate",
+              "Not yet validated",
+              "Died due to Covid")
+  ) 
+
+SSI_covid_resolved %>% 
+  ggplot() +
+  geom_bar(data = function(x) subset(x, Udfald != "Deaths 30 days after positive PCR"), stat = "identity", position = "stack",aes(YQ, value, fill = Udfald))+
+  zoo::scale_x_yearqtr(format = "%YQ%q", n = 5, expand = expansion(mult = c(0.025, .025))) +
+  facet_theme + 
+  labs(y = "Deaths",
+       title = "Deaths with positive SARS-CoV-2 PCR resolved for cause",
+       subtitle = "Weeks with <20 deaths are counted as zero and are unresolved as per SSI data file.\nQuarters with zero deaths in this chart had <150 covid deaths (pos PCR)",
+       caption = "Code and data at github.com/ktbaek/Excess-deaths-Denmark, data source: SSI")  +
+  theme(legend.title = element_blank(),
+        plot.background = element_rect(fill = "white"))
+
+ggsave("figures/ssi_covid_deaths.png", width = 18, height = 10, units = "cm", dpi = 300)
+  
+  
